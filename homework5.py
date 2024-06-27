b63d8f3a114bcffccc1f9ed7597950cf07bfd3b7
ChaynayaMyata <Iby.t-r@yandex.ru>
2024-06-27 17:44:39 +0300

    Update homework5.py

--- homework5.py
+++ homework5.py
@@ -1,8 +1,9 @@
 immutable_var=(1, 0.123, True, 'Кортеж который нельзя менять', ['Список который можно изменить', 7, False])
 print(immutable_var)
+print( )
 immutable_var[4][0]='Потому что можно менять списки'
-print(immutable_var)
+print('Immutable tuple: ', immutable_var)
 
 mutable_list=[1, 'kit', 0.79]
 mutable_list[0]=mutable_list[0]+7
-print(mutable_list)
+print('Mutable list: ',mutable_list)
