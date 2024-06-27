97fe62f4274460c8f3f1477180c0f3c6ca33170a
ChaynayaMyata <Iby.t-r@yandex.ru>
2024-06-27 14:56:00 +0300

    Add files via upload

---
+++ Homework2.py
@@ -0,0 +1,7 @@
+
+
+completed_DZ=12
+hours_spent=1.5
+Course_name='Python'
+Task_completion_time=(hours_spent/completed_DZ)
+print('Курс ',Course_name,', всего задач: ',completed_DZ,', затарчено часов: ',hours_spent,', среднее время выполнения: ',Task_completion_time)
